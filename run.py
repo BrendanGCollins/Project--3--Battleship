a8bd583c0ece116284e33fe7011a7a4ebd547466
BrendanGCollins <163342759+BrendanGCollins@users.noreply.github.com>
2024-06-20 18:03:10 +0000

    Add function to print board game and cell status

--- run.py
+++ run.py
@@ -11,3 +11,28 @@ class Battleship_game:
         #Create player board
         self.player_board = [["" for _ in range(self.board_size)] for _ in range (self.board_size)]
         self.computer_board = [["" for _ in range(self.board_size)] for _ in range(self.board_size)]
+
+
+    #Function to print game board
+    def print_board(self, board):
+        #Goes through each row on the board
+        for row in board:
+            #Empty string to build current row
+            row_string =""
+            #Goes through each cell on the board
+            for cell in row:
+                if cell == "":
+                    #Empty cells marked as ' UNKNOWN '
+                    row_string += " UNKNOWN "
+                elif cell == "H":
+                    #Hits marked as ' HIT '
+                    row_string +=" HIT "
+                elif cell == "M":
+                    #Misses marked as ' MISS '
+                    row_string += " MISS "
+                else:
+                    # if none of above add the letter of the ship on the cell.
+                    row_string += f" {cell} "
+            
+            #Print the row
+            print(row_string)
