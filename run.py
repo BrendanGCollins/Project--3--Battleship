32c906f836c6f870421ff49e63d6708bea616b8d
BrendanGCollins <163342759+BrendanGCollins@users.noreply.github.com>
2024-06-20 15:50:43 +0000

    Add game board and ships

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+#Game class
+class Battleship_game:
+    def __init__(self):
+        #Initalise game
+        #Set game board size
+        self.board_size = 5
+        #Add ships and their size
+        self.ships = {"A": 2, "B": 3, "C": 4, "D":5}
